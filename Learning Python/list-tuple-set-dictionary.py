cba16ad56df50f277e8ab030a11fc1e9fad380eb
jacobharrison93 <jacob.harrison93@gmail.com>
2023-03-06 21:32:18 -0600

    leap year exercise lesson # 9 on YouTube

--- Learning Python/list-tuple-set-dictionary.py
+++ Learning Python/list-tuple-set-dictionary.py
@@ -1,9 +1,18 @@
-# [ ] are list. Duplicate Values and are ordered
-#( ) are Tuples. Duplicate values, immutable
-# { } are sets. No Duplicated, mutable
-# {} are dictonaries. No duplicates or duplicate keys. {1:"a",2:"b"}
-# 
+month_days = [0,31,28,31,30,31,30,31,31,30,31]
 
-city = ['Austin','Dallas','Atlanta']
+def is_leap(year):
+    """return leap years"""
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
-print(city[-1:-2])
+def days_in_month(year,month):
+
+    if not 1 <= month <= 12:
+        return 'Invalid Month'
+    
+    if month == 2 and is_leap(year):
+        return 29
+    
+    return month_days[month]
+
+print(is_leap(2000))
+print(days_in_month(2000,2))
