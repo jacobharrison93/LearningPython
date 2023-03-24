b12525686c21da705d8c3d07d9d18f5de2845e35
jacobharrison93 <jacob.harrison93@gmail.com>
2023-03-23 21:40:37 -0500

    more timzezone stuff

--- Learning_Python/pythontest.py
+++ Learning_Python/pythontest.py
@@ -1,12 +1,17 @@
 import datetime
 import pytz
 
-dt = datetime.datetime(2023,3,24,12,30,45,tzinfo=pytz.UTC)
-print(dt)
+dt_utcnow = datetime.datetime.now(tz=pytz.UTC) #returns current local timezone of none
+print(dt_utcnow)
 
-dt_now = datetime.datetime.now(tz=pytz.UTC) #returns current local timezone of none
+dt_now = datetime.datetime.now()
+dt_east = dt_now.astimezone(pytz.timezone('US/Eastern'))
+centaral_tx = pytz.timezone('US/Central')
+
+dt_now = centaral_tx.localize(dt_now)
 print(dt_now)
 
-dt_utcnow = datetime.datetime.utcnow().replace(tzinfo = pytz.UTC)
-print(dt_now)
+dt_mtn = dt_utcnow.astimezone(pytz.timezone('US/Mountain'))
+print(dt_mtn)
+
 
