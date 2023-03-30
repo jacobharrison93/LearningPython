d950c0df71a6a119433f7bccc9f19ec2ed8d1ca2
jacobharrison93 <jacob.harrison93@gmail.com>
2023-03-29 19:22:13 -0500

    sort-list-tuple

--- Learning_Python/sort-list-tuples.py
+++ Learning_Python/sort-list-tuples.py
@@ -5,11 +5,13 @@ class Employee():
         self.salary = salary
     def __repr__(self):
         return f'({self.name},{self.age},${self.salary})'
-    
+from operator import attrgetter
+
 e1 = Employee('Carl',27,55000)
 e2 = Employee('Mark',55,100000)
 e3 = Employee('Sara',34,75000)
 
 employees = [e1,e2,e3]
 
-print(employees)
+s_employees = sorted(employees, key = attrgetter('age'))
+print(s_employees)
